07ee2befe0d3c8cc28c6ecb29e50fc9c33431b9d
Leonardo Andrés Crespo Sarango <lcrespos@est.ups.edu.ec>
2024-05-31 18:03:42 -0500

    creaciond e la clase de ventas(Sale) y la clase cliente(Client), ademas agrege en el app.properties funciones para el manejo de horas. en este caso la fecha se colocara con la fecha actual del sistema

--- src/main/java/ec/edu/ups/Bakend/BakendApplication.java
+++ src/main/java/ec/edu/ups/Bakend/BakendApplication.java
@@ -1,7 +1,11 @@
 package ec.edu.ups.Bakend;
 
+import ec.edu.ups.Bakend.Entity.Sale_Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 
 @SpringBootApplication
@@ -11,6 +15,10 @@ public class BakendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BakendApplication.class, args);
+		LocalDateTime fechaActual = LocalDateTime.now();
+		System.out.println("Fecha actual: " + fechaActual);
+		Sale_Entity sale = new Sale_Entity();
+		sale.setFecha_venta(fechaActual);
 	}
 
 
---
+++ src/main/java/ec/edu/ups/Bakend/Entity/Sale_Entity.java
@@ -0,0 +1,61 @@
+package ec.edu.ups.Bakend.Entity;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+public class Sale_Entity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long sale_id;
+    @Column(name = "sale_date")
+    private LocalDateTime fecha_venta;
+    @Column(name = "sale_clientID")
+    private String cliente_id;
+    @Column(name = "sale_total")
+    private double total;
+
+    public Sale_Entity() {}
+
+    public long getSale_id() {
+        return sale_id;
+    }
+
+    public void setSale_id(long sale_id) {
+        this.sale_id = sale_id;
+    }
+
+    public LocalDateTime getFecha_venta() {
+        return fecha_venta;
+    }
+
+    public void setFecha_venta(LocalDateTime fecha_venta) {
+        this.fecha_venta = fecha_venta;
+    }
+
+    public String getCliente_id() {
+        return cliente_id;
+    }
+
+    public void setCliente_id(String cliente_id) {
+        this.cliente_id = cliente_id;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "Sale_Entity{" +
+                "sale_id=" + sale_id +
+                ", fecha_venta=" + fecha_venta +
+                ", cliente_id='" + cliente_id + '\'' +
+                ", total=" + total +
+                '}';
+    }
+}

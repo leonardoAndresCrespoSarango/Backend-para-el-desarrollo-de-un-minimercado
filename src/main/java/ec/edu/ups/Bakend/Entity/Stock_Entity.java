aaf155123ffb8e68d8d05c523e7ac338421e6ec6
Leonardo Andrés Crespo Sarango <lcrespos@est.ups.edu.ec>
2024-05-31 19:24:06 -0500

    creacion de la clase stock

---
+++ src/main/java/ec/edu/ups/Bakend/Entity/Stock_Entity.java
@@ -0,0 +1,50 @@
+package ec.edu.ups.Bakend.Entity;
+import javax.persistence.*;
+
+
+@Entity
+public class Stock_Entity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long stock_id;
+    @Column(name = "stock_amount")
+    private long cantidad;
+    @Column(name = "stock_supplier")
+    private long proveedorid;
+
+    public Stock_Entity() {
+    }
+
+    public long getStock_id() {
+        return stock_id;
+    }
+
+    public void setStock_id(long stock_id) {
+        this.stock_id = stock_id;
+    }
+
+    public long getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(long cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public long getProveedorid() {
+        return proveedorid;
+    }
+
+    public void setProveedorid(long proveedorid) {
+        this.proveedorid = proveedorid;
+    }
+
+    @Override
+    public String toString() {
+        return "Stock_Entity{" +
+                "stock_id=" + stock_id +
+                ", cantidad=" + cantidad +
+                ", proveedorid=" + proveedorid +
+                '}';
+    }
+}
